5b8501a224d6f573f8fbc5706564e4969d07657b
Amy Paguirigan <amypaguirigan@gmail.com>
2019-04-30 13:36:44 -0700

    new caching data function

--- R/cromwellfunctions.R
+++ R/cromwellfunctions.R
@@ -1,14 +1,14 @@
-#' Pull Cromwell Jobs
+#' Pull Cromwell Workflow Jobs
 #'
-#' Requests metadata about Cromwell Jobs
+#' Requests metadata about Cromwell Workflow Jobs during a time period specified.
 #'
-#' @param days The number of days of history to return.
-#' @return Returns a long form data frame of metadata on workflows.
+#' @param days The number of days of history to return, defaults to 1 day.
+#' @return Returns a long form data frame of metadata on workflow jobs submitted
 #' @author Amy Paguirigan
 #' @details
-#' Requires valid cromwell URL to be set in the environment.
+#' Requires valid Cromwell URL to be set in the environment.
 #' @export
-cromwellJobs <- function(days = 7){
+cromwellJobs <- function(days = 1){
   if ("" %in% Sys.getenv("CROMWELLURL")) {
     print("The cromwell URL is not set.  Please setCromwellURL().")} else print("Cromwell URL set successfully.")
   print("cromwellJobs(); Querying cromwell for jobs list.")
@@ -24,15 +24,15 @@ cromwellJobs <- function(days = 7){
   return(cromTable)
 }
 
-#' Pull Cromwell Calls
+#' Pull Cromwell Workflow Calls
 #'
 #' Retrieve and process call data for a workflow
 #'
-#' @param workflow_id The workflow ID to return calls for.
+#' @param workflow_id The workflow ID to return call metadata on.
 #' @return Returns a long form data frame of metadata on calls.
 #' @author Amy Paguirigan
 #' @details
-#' Requires valid cromwell URL to be set in the environment.
+#' Requires valid Cromwell URL to be set in the environment.
 #' @export
 cromwellCall <- function(workflow_id) {
   if ("" %in% Sys.getenv("CROMWELLURL")) {
@@ -58,9 +58,10 @@ cromwellCall <- function(workflow_id) {
         as.data.frame(Y)})
       names(simpleStan) <- names(stanley)
       jobdf <- purrr::reduce(simpleStan, dplyr::bind_rows)
-      #If a callNames value is in teh string in callName, then replace that value with THAT callNames value
+      #If a callNames value is in the string in callName, then replace that value with THAT callNames value
       jobdf$callName <- gsub("[0-9]*$", "", jobdf$callName)
-    }}
+    }
+    } else(jobdf = as.data.frame(paste0("There are no calls associated with the workflow_id: ", workflow_id)))
   return(jobdf)
 }
 
@@ -69,10 +70,10 @@ cromwellCall <- function(workflow_id) {
 #' Retrieve and process all labels, submission and workflow level metadata for a workflow
 #'
 #' @param workflow_id The workflow ID to return metadata for.
-#' @return Returns a long form data frame of metadata on workflows.
+#' @return Returns a long form data frame of metadata on a workflow.
 #' @author Amy Paguirigan
 #' @details
-#' Requires valid cromwell URL to be set in the environment.
+#' Requires valid Cromwell URL to be set in the environment.
 #' @export
 cromwellWorkflow <- function(workflow_id) {
   if ("" %in% Sys.getenv("CROMWELLURL")) {
@@ -96,7 +97,7 @@ cromwellWorkflow <- function(workflow_id) {
     if ("end" %in% colnames(resultdf) == T & "start" %in% colnames(resultdf)==T ) {
       resultdf <- dplyr::mutate(resultdf, workflowDuration = as.character(difftime(end, start, units = "mins")))
     } else {resultdf <- dplyr::mutate(resultdf, end = "NA" , workflowDuration = "NA")}
-    }
+    } else(resultdf = as.data.frame(paste0("There are no available metadata associated with the workflow_id: ", workflow_id)))
   return(resultdf)
 }
 #' Pull Cromwell Call Failure Data
@@ -115,24 +116,84 @@ cromwellFailures <- function(workflow_id){
     print("The cromwell URL is not set.  Please setCromwellURL().")} else print("Cromwell URL set successfully.")
   cromfail <- httr::content(httr::GET(paste0(Sys.getenv("CROMWELLURL"),"/api/workflows/v1/",
                                              workflow_id,"/metadata?includeKey=failures&includeKey=jobId")))
-  meltedlists <- as.data.frame(c())
+  faildf <- as.data.frame(c())
+
   if(is.list(cromfail$calls)==T){
-    bobfail <- unlist(cromfail$calls, recursive = FALSE, use.names = TRUE)
+    callNames <- names(cromfail$calls)
+    bobfail <- purrr::pluck(cromfail, "calls")
     if (length(bobfail) > 0) {
-      todfs <- purrr::map(names(bobfail), function(i) {
-        Z <- as.data.frame(rbind(unlist(bobfail[[i]])), stringsAsFactors = F)
-        Z$callName <- i
-        Z})
-      meltedlists <- purrr::reduce(todfs, dplyr::bind_rows)
-      meltedlists$callName <- gsub("[0-9]*$", "", meltedlists$callName)
-      meltedlists$workflow_id <- workflow_id
-      if ("failures.message" %in% colnames(meltedlists)) {
-        meltedlists <- dplyr::filter(meltedlists,is.na(failures.message) == F)} else {
-          meltedlists <- meltedlists[0,]
+      faildf <- purrr::map(bobfail, function(callData) {
+        purrr::map_dfr(callData, function(shardData){
+          Z <- as.data.frame(rbind(unlist(shardData)))
+        })
+      }) %>% purrr::map_dfr(., function(x){x}, .id = "callName")
+      faildf$workflow_id <- workflow_id
+      if ("failures.message" %in% colnames(faildf)) {
+        faildf <- dplyr::filter(faildf,is.na(failures.message) == F)} else {
+          faildf <- faildf[0,]
         }
-     }
-  }
+    }
+  } else(faildf = as.data.frame(paste0("There are no failure metadata associated with the workflow_id: ", workflow_id)))
+  return(faildf)
 }
+
+#' Pull Cromwell Call Caching Data
+#'
+#' Gets info about call caching status for the calls of a workflow
+#'
+#' @param workflow_id The workflow ID to return call caching metadata for.
+#' @return Returns a long form data frame of metadata on call caching in a workflow.
+#' @author Amy Paguirigan
+#' @details
+#' Requires valid Cromwell URL to be set in the environment.
+#' @export
+#
+cromwellCache <- function(workflow_id){
+  if ("" %in% Sys.getenv("CROMWELLURL")) {
+    print("The cromwell URL is not set.  Please setCromwellURL().")} else print("Cromwell URL set successfully.")
+  crommetadata <- httr::content(httr::GET(paste0(Sys.getenv("CROMWELLURL"),"/api/workflows/v1/",
+                                                 workflow_id,"/metadata?expandSubWorkflows=false")), as = "parsed")
+  geocache <- as.data.frame(c())
+  if(is.list(crommetadata$calls)==T){ # if there are calls to be queried, continue
+    bobCalls <- purrr::pluck(crommetadata, "calls") # we only want the calls data from the metadata for this workflow
+    bobCallMeta <- purrr::map(bobCalls, function(x){ # for each of the calls in the workflow...
+      purrr::map_dfr(x , function(y){ # and for each of the shards in that workflow...
+        a <- purrr::keep(y, names(y) %in% c("callCaching", "inputs", "outputs")) # select only these lists
+        b <- as.data.frame(rbind(unlist(a))) # flatten them and make them a data frame
+        b$shardIndex <- y$shardIndex # add the shard Index associated
+        b$executionStatus <- y$executionStatus # and this
+        b$returnCode <- y$returnCode # and this
+        b$jobId <- y$jobId # and especially this
+        b<- b %>% dplyr::select(-dplyr::starts_with("callCaching.hitFailures")) # then remove any data from the messy hitFailures lists
+        return(b)
+      })
+    })
+    cacheHits <- purrr::map_dfr(bobCallMeta, function(x){
+      x %>% dplyr::filter(callCaching.hit == T) %>% Filter(function(x)!all(is.na(x)), .)
+    }, .id = "callName")
+    cacheMisses <- purrr::map_dfr(bobCallMeta, function(x){
+      x %>% dplyr::filter(callCaching.hit == F) %>% Filter(function(x)!all(is.na(x)), .)
+    }, .id = "callName")
+
+    hitFailures <- purrr::map(bobCalls, function(eachCall){
+      listofShardFrames <- purrr::map_dfr(eachCall, function(eachShard){
+        c <- purrr::pluck(eachShard, "callCaching")
+        d <- as.data.frame(unlist(purrr::pluck(c, "hitFailures")))
+        if(nrow(d)>0){
+          colnames(d) <- c("hitFailureMessage")
+          d$breadCrumbs <- rownames(d)
+          d$shardIndex <- eachShard$shardIndex
+          return(d)
+        }
+      })
+    }) %>% purrr::map_dfr(., function(x){ x }, .id = "callName") #WTF?? Why does this work but reduce or flatten don't?
+
+    cacheMisses <- dplyr::full_join(cacheMisses, cacheHitFailures, by = c("callName", "shardIndex"))
+    geocache <- dplyr::bind_rows(cacheHits, cacheMisses)
+    } else(geocache = as.data.frame(paste0("There are no calls associated with the workflow_id: ", workflow_id)))
+  return(geocache)
+  }
+
 
 
 
